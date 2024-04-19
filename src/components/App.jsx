d46151ce5920998724807c350d7911ccd9aae1b9
Wiola-D <wioletta.d.work@gmail.com>
2024-04-19 09:24:56 +0200

    Add filter and Contactlist

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,26 @@
 import { useState } from 'react';
 import { ContactForm } from './Phonebook';
+import { ContactList } from './ContactList';
 
 export const App = () => {
   const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
 
   const addContact = event => {
     setContacts(prevContacts => [...prevContacts, event]);
   };
 
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toUpperCase().includes(filter.toUpperCase())
+  );
+
   return (
     <div>
       <h1>Phonebook</h1>
       <ContactForm contacts={contacts} addContact={addContact} />
+      <h2>Contacts</h2>
+
+      <ContactList contacts={filteredContacts} />
     </div>
   );
 };
---
+++ src/components/ContactList.jsx
@@ -0,0 +1,11 @@
+export const ContactList = ({ contacts }) => {
+  return (
+    <ul>
+      {contacts.map(contact => (
+        <li key={contact.id}>
+          <p>{contact.name}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
