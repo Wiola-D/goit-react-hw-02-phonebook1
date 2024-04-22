467cd23ac998cfa188bb837d73be7e6bc112c51e
Wiola-D <wioletta.d.work@gmail.com>
2024-04-22 11:50:58 +0200

    add example contact

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,6 +1,7 @@
 export const ContactList = ({ contacts, removeContact }) => {
   return (
     <ul>
+      {contacts.length === 0 && <li>Jan Kowalski: 123-456-789</li>}
       {contacts.map(contact => (
         <li key={contact.id}>
           <p>
